e347db7b41bc25639e078789f75ced61b4390665
JGanning <41811665+JGanning@users.noreply.github.com>
2020-03-24 08:51:09 -0400

    attempting dynamic headers

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { headers } from '../temp/data'
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  headers = headers;
+  
+  constructor() {
+    
+   }
 
   ngOnInit() {
   }
---
+++ src/app/temp/data.ts
@@ -0,0 +1,7 @@
+export const headers = [
+    'This Person We Are About To Interview Claims To Be Taylor Swift. She Better Not Be Fucking Lying.',
+    'Inspiring: Terminally Ill Child Breaks Record For Most Consecutive Coughs',
+    '4 Cereal Mascots From Your Childhood That Are Probably Still Politically Correct',
+    'When Animals Attack: Woman Savagely Mauled by Swarm of Amoebas',
+    'Dave Grohl Owes Me $5',
+]
